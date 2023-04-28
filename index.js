4dc5a430f6f304a89acaf10bd2702a01980fc247
Wilfredo <drastic87@gmail.com>
2023-04-27 21:23:57 -0400

    deployment fix attempt

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/api/messages', require('./routes/messages'));
 app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.js'));
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.jsx'));
 });
 
 app.listen(port, () => {
